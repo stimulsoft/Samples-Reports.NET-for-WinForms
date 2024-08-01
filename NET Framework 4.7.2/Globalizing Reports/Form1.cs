e8733110269050680c42b08b5a99a77dc650385f
Uladzimir Khotchanka <hotchenko@gmail.com>
2024-08-01 17:57:58 +0200

    fixed main forms

--- NET Framework 4.7.2/Globalizing Reports/Form1.cs
+++ NET Framework 4.7.2/Globalizing Reports/Form1.cs
@@ -69,7 +69,6 @@ namespace Globalizing_Reports
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.cbCountry = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
@@ -94,19 +93,8 @@ namespace Globalizing_Reports
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 25);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Preview";
+            this.button2.Text = "Show";
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(53, 82);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 25);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Design";
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -168,15 +156,6 @@ namespace Globalizing_Reports
 
 			Application.EnableVisualStyles();
 			Application.Run(new Form1());
-		}
-
-		private void button1_Click(object sender, System.EventArgs e)
-		{
-			StiReport report = new StiReport();
-			
-			report.RegData(dataSet1);
-			report.Load("..\\GlobalizedSimpleList.mrt");
-			report.Design();
 		}
 
 		private void button2_Click(object sender, System.EventArgs e)
